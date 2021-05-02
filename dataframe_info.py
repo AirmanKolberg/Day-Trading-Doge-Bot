7147f49b68396645d27ece895593118f7bd2991a
T.J. Kolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-05-01 20:44:52 -0600

    🤫 Secrets and Data for DF-Maker

---
+++ dataframe_info.py
@@ -0,0 +1,19 @@
+# The following variables will differ per your DataFrame
+average_tag = 'Daily % Gain'
+assets_tag = 'Total Managed Assets'
+pre_num = 'Total (past '
+post_num = ' days):'
+trading_dataframe = 'BitCoin-Trading-Data.csv'
+
+# List of average Daily Percentage Yields for DataFrame columns
+potential_growths = [1.01, 1.011, 1.012, 1.013, 1.014, 1.015]
+
+"""
+The data below can be put in manually, or automated via something
+like robin_stocks, for example.
+"""
+
+secret_crypto = 0
+secret_buying_power = 0
+secret_assets_offset = 0
+secret_initial_assets = 0
